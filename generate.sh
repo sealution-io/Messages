1658c26243a69b1ba3c0e4f7c229fa2fcf0eb922
Anthony <aamining2@gmail.com>
2022-10-20 11:55:48 +0200

    generate proper namespace

--- generate.sh
+++ generate.sh
@@ -1,1 +1,4 @@
-php ./vendor/bin/protobuf --include-descriptors -i . -o ./src/ ./src/mainmessage.proto
+composer install &&
+php ./vendor/bin/protobuf --include-descriptors -i . -o ./ ./mainmessage.proto &&
+mv ./Sealution/Messages ./Messages &&
+rm -rf ./Sealution
