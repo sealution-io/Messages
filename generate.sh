176e88a120d50e259d27a6a44c658683d5147bec
Anthony <aamining2@gmail.com>
2022-10-20 12:01:20 +0200

    should work

--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
 composer install &&
 php ./vendor/bin/protobuf --include-descriptors -i . -o ./ ./mainmessage.proto &&
-mv ./Sealution/Messages ./Messages &&
+mv ./Sealution/Messages ./src &&
 rm -rf ./Sealution
