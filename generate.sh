00a388c138889efea1c05c23604f9b21b066d9fe
Anthony <aamining2@gmail.com>
2022-10-20 13:14:00 +0200

    another possible fix

--- generate.sh
+++ generate.sh
@@ -1,6 +1,8 @@
 composer install &&
 rm -rf ./src &&
 rm -rf ./GPBMetadata &&
-php ./vendor/bin/protobuf --include-descriptors -i . -o ./ ./mainmessage.proto &&
+protoc --php_out=./ mainmessage.proto &&
+mkdir -p ./src/Messages &&
 mv ./Sealution/Messages ./src &&
+mv ./GPBMetadata ./src/GPBMetadata &&
 rm -rf ./Sealution
